7d0e4cd402383b4e90c708ae5eda3125a2431d84
Lucas Condon <lcondon@bennington.edu>
2018-09-18 22:08:35 -0400

    upd8

--- src/components/StrangerCard/StrangerCard.js
+++ src/components/StrangerCard/StrangerCard.js
@@ -3,13 +3,13 @@ import './Strange.css';
 
 const StrangerCard = props => {
   return (
-    <div className="col-3 my-1 p-0" key={props.id}>
+    <div className="col-3 my-1 py-1" key={props.id}>
       <img
         onClick={e => {
           e.preventDefault();
           props.remove(props.id, e);
         }}
-        className="img-thumbnail click-item"
+        className="img-thumbnail click-item hvr-grow-shadow"
         alt={props.name}
         src={props.image}
       />
